666d2adaf9ef2853aaed31a5391f2d9c65b6be3d
Aregue RH <arah@hotmail.fr>
2024-06-26 00:30:19 +0200

    la page edit et le bouton envoyer la modif sont opérationnels

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Produits;
+use App\Form\ProductType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,4 +22,26 @@ class ProductController extends AbstractController
             'products' => $products,
         ]);
     }
+
+
+    #[Route('/products/{id}/edit', name:'product.edit', methods: ['GET', 'POST'])]
+    public function edit(Produits $produit, Request $request, EntityManagerInterface $em) {
+        $formProd = $this->createForm(ProductType::class, $produit);
+        $formProd->handleRequest($request);
+
+        if ($formProd->isSubmitted() && $formProd->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'la recette a bien été modifié');
+            return $this->redirectToRoute('recipe.index');
+        }
+
+        return $this->render('product/edit.html.twig', 
+        [
+            'produit' => $produit,
+                'formProd' => $formProd->createView()
+            
+        ]);
+    }
 }
+
+
