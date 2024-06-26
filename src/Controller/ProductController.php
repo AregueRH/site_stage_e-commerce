a23a36bd2509b6e3e361366f5d21014ea48d6aa6
Aregue RH <arah@hotmail.fr>
2024-06-26 22:02:57 +0200

    btn ajout, sup et modif operationnels

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Produits;
 use App\Form\ProductType;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,8 +32,8 @@ class ProductController extends AbstractController
 
         if ($formProd->isSubmitted() && $formProd->isValid()) {
             $em->flush();
-            $this->addFlash('success', 'la recette a bien été modifié');
-            return $this->redirectToRoute('recipe.index');
+            $this->addFlash('success', 'la recette a bien été modifiée');
+            return $this->redirectToRoute('products_list');
         }
 
         return $this->render('product/edit.html.twig', 
@@ -42,6 +43,51 @@ class ProductController extends AbstractController
             
         ]);
     }
+    #[Route('products/create' , name : 'produit.create')]
+    public function create(Request $request, EntityManagerInterface $em) 
+    {
+        $produit = new Produits();
+        $formulaireProd = $this->createForm(ProductType::class, $produit) ;
+        $formulaireProd->handleRequest($request);
+
+        if ($formulaireProd->isSubmitted() && $formulaireProd->isValid()) {
+                 $em->persist($produit);
+            $em->flush();
+            $this->addFlash('success', 'la recette a bien été créée');
+            return $this->redirectToRoute('products_list');
+        }
+        
+        return $this->render('product/create.html.twig', [
+            'formProd' => $formulaireProd
+        ]);
+        
+    }
+
+    #[Route('products/{id}/edit', name: 'produit.delete', methods: ['DELETE'])]
+    public function remove(Produits $produit, EntityManagerInterface $em)
+    {
+        $em->remove($produit);
+        $em->flush();
+        $this->addFlash('success', 'Le produit a bien été modifié');
+        return $this->redirectToRoute('products_list');
+    }
 }
 
 
+// #[Route('recettes/create', name: 'recipe.create')]
+//     public function create2(Request $request, EntityManagerInterface $em) 
+//     {
+//         $recipe = new Recipe();
+//         $form = $this->createForm(RecipeType::class, $recipe);
+//         $form->handleRequest($request);
+//         if ($form->isSubmitted() && $form->isValid()) {
+            
+//             $em->persist($recipe);
+//             $em->flush();
+//             $this->addFlash('success', 'la recette a bien été créée');
+//             return $this->redirectToRoute('recipe.index');
+//         }
+//         return $this->render('recipe/create.html.twig', [
+//             'form' => $form
+//         ]);
+//     }
