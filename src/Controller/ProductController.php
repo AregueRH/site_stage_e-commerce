ade958b81da5979fe53a4b8d3da0b2270bfa909b
Aregue RH <arah@hotmail.fr>
2024-07-02 20:39:48 +0200

    session USER et ADMIN opérationnels, pages products&cie restreintes au rôle ADMIN

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,11 +9,14 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
     #[Route('/products', name: 'products_list')]
+    #[IsGranted('ROLE_ADMIN')]
+    
     public function index(EntityManagerInterface $em): Response
     {
         // pour récupérer tout les produits
@@ -23,27 +26,29 @@ class ProductController extends AbstractController
             'products' => $products,
         ]);
     }
-
-
+    
+    
     #[Route('/products/{id}/edit', name:'product.edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_ADMIN')]
     public function edit(Produits $produit, Request $request, EntityManagerInterface $em) {
         $formProd = $this->createForm(ProductType::class, $produit);
         $formProd->handleRequest($request);
-
+        
         if ($formProd->isSubmitted() && $formProd->isValid()) {
             $em->flush();
             $this->addFlash('success', 'la recette a bien été modifiée');
             return $this->redirectToRoute('products_list');
         }
-
+        
         return $this->render('product/edit.html.twig', 
         [
             'produit' => $produit,
-                'formProd' => $formProd->createView()
+            'formProd' => $formProd->createView()
             
         ]);
     }
     #[Route('products/create' , name : 'produit.create')]
+    #[IsGranted('ROLE_ADMIN')]
     public function create(Request $request, EntityManagerInterface $em) 
     {
         $produit = new Produits();
@@ -62,8 +67,9 @@ class ProductController extends AbstractController
         ]);
         
     }
-
+    
     #[Route('products/{id}/edit', name: 'produit.delete', methods: ['DELETE'])]
+    #[IsGranted('ROLE_ADMIN')]
     public function remove(Produits $produit, EntityManagerInterface $em)
     {   
         $em->remove($produit);
@@ -72,22 +78,3 @@ class ProductController extends AbstractController
         return $this->redirectToRoute('products_list');
     }
 }
-
-
-// #[Route('recettes/create', name: 'recipe.create')]
-//     public function create2(Request $request, EntityManagerInterface $em) 
-//     {
-//         $recipe = new Recipe();
-//         $form = $this->createForm(RecipeType::class, $recipe);
-//         $form->handleRequest($request);
-//         if ($form->isSubmitted() && $form->isValid()) {
-            
-//             $em->persist($recipe);
-//             $em->flush();
-//             $this->addFlash('success', 'la recette a bien été créée');
-//             return $this->redirectToRoute('recipe.index');
-//         }
-//         return $this->render('recipe/create.html.twig', [
-//             'form' => $form
-//         ]);
-//     }
