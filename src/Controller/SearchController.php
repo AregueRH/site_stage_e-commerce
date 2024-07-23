f7dbb77af254ada5d034675e031e703c6ed3ff9c
Aregue RH <arah@hotmail.fr>
2024-07-23 15:21:49 +0200

    maj entree site et front

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
+use App\Form\SearchType;
 use App\Repository\ProduitsRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Form\SearchType;
 
 class SearchController extends AbstractController
 {
     #[Route('/search', name: 'app_search')]
+    #[IsGranted('ROLE_USER')]
+
     public function search(Request $request, ProduitsRepository $produitsRepository): Response
     {
         $form = $this->createForm(SearchType::class);
